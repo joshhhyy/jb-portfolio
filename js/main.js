1828d0a1aae7c919f6631d05bca57738f601f288
Josh Bonador <Josh@192-168-1-11.tpgi.com.au>
2015-07-27 22:28:02 +1000

    Pushing existing files

---
+++ js/main.js
@@ -0,0 +1,56 @@
+$(document).ready(function() {
+
+  defaultMessage = 'Adding more and more by the second:'
+
+  $('#skills').on('click', function(e) {
+    e.preventDefault();
+    $('.info').animate({scrollTop: 300})
+  })
+
+  $('#js').hover(function() {
+    $('.skillLevel').html("Easily my most confident language. But I'm eager to learn more about this beat")
+  }, function() {
+    $('.skillLevel').html(defaultMessage)
+  })
+
+  $('#jquery').hover(function() {
+    $('.skillLevel').html("Makes DOM manipulation SO much easier! Gotta love it when things get easier")
+  }, function() {
+    $('.skillLevel').html(defaultMessage)
+  })
+
+  $('#html5').hover(function() {
+    $('.skillLevel').html("Fundamentals! Everyday I'm learning more and getting better with structured design")
+  }, function() {
+    $('.skillLevel').html(defaultMessage)
+  })
+
+  $('#css3').hover(function() {
+    $('.skillLevel').html("I often aim for sleek and minimalist design but I'm completely fine with producing something beyond that.")
+  }, function() {
+    $('.skillLevel').html(defaultMessage)
+  })
+
+  $('#ruby').hover(function() {
+    $('.skillLevel').html("Extremely confident in Back-end Development and the Ruby language! You can catch me RoRo meetups as well!")
+  }, function() {
+    $('.skillLevel').html(defaultMessage)
+  })
+
+  $('#rails').hover(function() {
+    $('.skillLevel').html("One of my favourite frameworks! Wouldn't know what to do without it! Big advocate for Test-Driven Development")
+  }, function() {
+    $('.skillLevel').html(defaultMessage)
+  })
+
+  $('#github').hover(function() {
+    $('.skillLevel').html("My favourite resource for contributing, learning and getting familiar with open source projects! ")
+  }, function() {
+    $('.skillLevel').html(defaultMessage)
+  })
+
+  $('#about').on('click', function(e) {
+    e.preventDefault();
+    $('.info').animate({scrollTop: 0})
+  })
+})
